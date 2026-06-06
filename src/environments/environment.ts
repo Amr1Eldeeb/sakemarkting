6afe43449166cf5d2f6c953131714d742b27f328
Amr Khaled <mrmrmmrmmm@gmail.com>
2026-06-06 15:49:59 +0300

    enbaled https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5098/api',
+  apiUrl: 'https://sake-soluations.runasp.net/api',
   apiUrlProd: 'https://sake-soluations.runasp.net/api',
   imageBaseUrl: 'http://localhost:5098',
   apiEndpoints: {
